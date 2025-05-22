2a84a6107a0e3e2e6026db4268a86a63c338b0a6
opixxx <opix0306@naver.com>
2025-05-22 16:18:18 +0900

    chore : 초기 셋팅

---
+++ src/test/java/com/opixxx/ecommerce/EcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.opixxx.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
