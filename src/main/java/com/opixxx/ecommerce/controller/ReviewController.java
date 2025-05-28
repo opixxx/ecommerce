63dce8d692d799e848d13700c1ebe1db4c200726
opixxx <opix0306@naver.com>
2025-05-28 14:51:33 +0900

    style : 사용안하는 import 제거

--- src/main/java/com/opixxx/ecommerce/controller/ReviewController.java
+++ src/main/java/com/opixxx/ecommerce/controller/ReviewController.java
@@ -1,6 +1,5 @@
 package com.opixxx.ecommerce.controller;
 
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
